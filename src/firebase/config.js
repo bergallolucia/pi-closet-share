0fc311f6c96811997d00ebb2c5b558335c24bcdd
bergallolucia <bergallol@udesa.edu.ar>
2026-06-01 20:51:21 -0300

    instale la base

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxicFihONe6GjCdgMiFqSwovvQnSnfk6w",
+  authDomain: "closetshare-pi.firebaseapp.com",
+  projectId: "closetshare-pi",
+  storageBucket: "closetshare-pi.firebasestorage.app",
+  messagingSenderId: "82926536344",
+  appId: "1:82926536344:web:23af9d86dc5db4d958b2ed"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+const app = initializeApp(firebaseConfig);
