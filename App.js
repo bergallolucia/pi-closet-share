5844b09378b8819face8c3dbb72a1ab491eaae16
bergallolucia <bergallol@udesa.edu.ar>
2026-06-06 13:23:09 -0300

    menu de navegación

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, Navigationcontainer } from '@react-navigation/native'; 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from "./src/screens/Login"; 
+import Register from "./src/screens/Register"; 
+import HomeMenu from "./src/components/HomeMenu"; 
+import Comments from "./src/screens/Comments"; 
+
+const Stack = createNativeStackNavigator(); 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Resgister" component={Register} />
+        <Stack.Screen name="HomeMenu" component={HomeMenu} />
+        <Stack.Screen name="Comments" component={Comments} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
